3289b0ec1f22b757c021f95b25b8440f99177e1a
vagrant <vagrant@vagrant.vm>
2018-09-05 00:55:29 +0000

    catalog before define bootstrap4 completely

--- catalog.py
+++ catalog.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
+
 from flask import Flask, request, redirect, url_for
 
-from forumdb import get_posts, add_post
+from catalogdb import get_posts, add_post
 
 app = Flask(__name__)
 
@@ -9,7 +12,14 @@ HTML_WRAP = '''\
 <!DOCTYPE html>
 <html>
   <head>
-    <title>DB Forum</title>
+    <title>DB Catalog in Bootstrap</title>
+    <title>Bootstrap 4 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <style>
       h1, form { text-align: center; }
       textarea { width: 400px; height: 100px; }
@@ -21,15 +31,101 @@ HTML_WRAP = '''\
     </style>
   </head>
   <body>
+   <header class="header"> Header</header>
+   <div class"sidebar"> 
+    <div class"sidebar-wrapper"> </div>
+   </div>
+   <!--Main Container>
+  <main class="main-container">
+   <section class="main-highlight">
+    <div class="highlight-carousel slider-carousel">
+     <div class="owl-carousel owl-loaded owl-drag" id="postCarousel">
+      <div class="owl-stage-outer">
+       <div class="owl-stage" style="transform:
+       translate3d(-1913px, 0px, 0px); transition:all 0s ease 0s; width: 7655px;">
+        <div class="owl-tem cloned" style="width:936.8px; margin-right: 20px;"> </div>
+        <div class="owl-tem cloned" style="width:936.8px; margin-right: 20px;"> </div>
+        <div class="owl-tem active" style="width:936.8px; margin-right: 20px;"> </div>
+         <div class="item">
+          <article class="post-box" style="background-image:url();">
+           <div class="post-overlay"> </div>
+           <a href="#" class="post-overlayLink"></a>
+          </article>
+         </div>
+        </div>
+        <div class="owl-item" style="width: 936.8px; margin-right: 20px;"></div>
+         <div class="item"></div>
+        </div>
+        <div class="owl-item" style="width: 936.8px; margin-right: 20px;"></div>
+         <div class="item"></div>
+        </div>
+        <div class="owl-item" style="width: 936.8px; margin-right: 20px;"></div>
+         <div class="item"></div>
+        </div>
+        <div class="owl-item cloned" style="width: 936.8px; margin-right: 20px;"></div>
+         <div class="item"></div>
+        </div>
+        <div class="owl-item cloned" style="width: 936.8px; margin-right: 20px;"></div>
+         <div class="item"></div>
+         ::after
+        </div>
+       </div>
+       <div class="owl-nav"></div>
+        <button type="button" role="presentation" class="owl-prev">
+        </button>
+        <button type="button" role="presentation" class="owl-next">
+        </button> 
+        </div>
+        <div class="owl-dots">
+         <button role="button" class="owl-dot active">></button>
+         <button role="button" class="owl-dot"></button>
+         <button role="button" class="owl-dot"></button>
+         <button role="button" class="owl-dot"></button>
+        </div>
+       </div>
+      </div>
+     </section>
+     <section class="main-content"> </selection>
+      <div class="main-content-wrapper"></div>
+     </section>
+    </main>
+    <!-- register -->
+    <div class="m-modal-box" id="registerModal"></div>
+     <div class="m-modal-overlay"></div>
+     <div class="m-modal-content small"> </div>
+    </div>
+    
+    <div class="m-modal-box" id="loginModal"></div>
+     <div class="m-modal-overlay"></div>
+     <div class="m-modal-content small"> </div>
+    </div>
+
+    <div class="m-modal-box" id="newsletterModal"></div>
+     <div class="m-modal-overlay"></div>
+     <div class="m-modal-content small"> </div>
+    </div>
+    <div class="overlay"> </div>
+  
+
+<div class="container-fluid">
+
     <h1>DB Forum</h1>
+     <div class="container"
     <form method=post>
       <div><textarea id="content" name="content"></textarea></div>
       <div><button id="go" type="submit">Post message</button></div>
     </form>
+    </div>
     <!-- post content will go here -->
 %s
+</div>
+
+ <script src="plugins/zebra-tooltip/zebra_tooltips.min.js"></script>
+    <script src="plugins/owl-carousel/owl.carousel.min.js"></script>
+    <script type="text/javascript"></script>
   </body>
 </html>
+</iframe>
 '''
 # HTML template for an individual comment
 POST = '''\
--- catalogdb.py
+++ catalogdb.py
@@ -1,30 +1,15 @@
-# "Database code" for the DB Forum, connecting to real DB=Forum.
+# "Database code" for the DB Forum.
 
 import datetime
-import psycopg2, bleach
 
-DBNAME = "forum"
-
-# old: POSTS = [("This is the first post.", datetime.datetime.now())]
+POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
-  
-      """Return all posts from the 'database', most recent first."""
-#old  return reversed(POSTS)
-
-      db = psycopg2.connect(database=DBNAME)
-      c  = db.cursor()
-      c.execute("select content, time from posts order by time desc")
-      posts = c.fetchall()
-      db.close()
+  """Return all posts from the 'database', most recent first."""
+  return reversed(POSTS)
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  #POSTS.append((content, datetime.datetime.now()))
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("insert into posts values (%s)", (bleach.clean(content),))  # good
-  db.commit()
-  db.close()
+  POSTS.append((content, datetime.datetime.now()))
 
 
