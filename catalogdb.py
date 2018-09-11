1aee1c7d260a853a00dabc6b66caea33af14c627
vagrant <vagrant@vagrant.vm>
2018-09-11 03:53:49 +0000

    Review1: query3, pep8

--- catalogdb.py
+++ catalogdb.py
@@ -1,5 +1,6 @@
 #!/bin/python3
-import psycopg2, bleach
+import psycopg2
+import bleach
 import datetime
 
 
@@ -14,8 +15,6 @@ q1_results = dict()
 q1_results['title'] = (
     "1. The most popular three articles of all time?")
 q1_results['views'] = " views"
-#-------------------------------------------------------------
-
 q2 = ("""SELECT authors.name, count(*) AS num
       FROM articles
       JOIN authors on articles.author = authors.id
@@ -26,46 +25,75 @@ q2_results = dict()
 q2_results['title'] = (
     "2. Who are the most popular article authors of all time?")
 q2_results['views'] = " views"
-#----------------------------------------------------------
+q3 = ("""SELECT to_char(errors.date, 'Mon DD, YYYY'),
+      round( cast(errors.percent as numeric), 3) FROM percent_error errors;""")
+q3_results = dict()
+q3_results['title'] = (
+    "3. On which days did more than 1% of requests lead to errors?")
+q3_results['views'] = ""
+q3_v1 = (""" CREATE VIEW error AS
+    SELECT date(time), count(status)
+    FROM log
+    WHERE status != '200 OK'
+    GROUP BY date(time)
+    ORDER BY count(status);  """)
 
-# Get query results from databse and return them
+
+q3_v2 = (""" CREATE VIEW full_view AS
+    SELECT date(time), count(status)
+    FROM log
+    GROUP BY date(time)
+    ORDER BY count(status);""")
+
+q3_v3 = ("""CREATE VIEW query_3 AS
+    SELECT errors.date, e.count AS errors, errors.count AS all
+    FROM full_view errors
+    LEFT JOIN error e
+    ON errors.date=e.date; """)
+q3_v4 = ("""CREATE VIEW percent_error AS
+    SELECT errors.date, CAST (errors.errors as float) /
+    errors.all * 100 AS percent
+    FROM query_3 errors
+    WHERE CAST (errors.errors as FLOAT) / errors.all * 100 > 1; """)
+
+
 def get_results(query):
+
+    '''connecting to database'''
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
+    c.execute(q3_v1)
+    c.execute(q3_v2)
+    c.execute(q3_v3)
+    c.execute(q3_v4)
+    c.execute(query)
     results = c.fetchall()
     db.close()
     return results
 
-# Print results method
-def print_results(query_results):
-    print (q['title'])
-    for r in q['results']:
-        print ('\t' + str(r[0]) + ': ' +
-               str(r[1]) +
-               q['views'])
 
-# Store query results
+def print_results(q):
+    print(q['title'])
+    for r in q['results']:
+        print('\t' + str(r[0]) + ': ' + str(r[1]) + q['views'])
+
+
 q1_results['results'] = get_results(q1)
 q2_results['results'] = get_results(q2)
-
-
-# Print query results
+q3_results['results'] = get_results(q3)
 
 print_results(q1_results)
 print_results(q2_results)
+print_results(q3_results)
 
-#-------------------------------------------------
-#defining get posts 
+
 def get_posts1():
     return q1_results['results']
-    #return q1_results.items()
-  
+
+
 def get_posts2():
     return q2_results['results']
 
 
-
-
-
-
-
+def get_posts3():
+    return q3_results['results']
